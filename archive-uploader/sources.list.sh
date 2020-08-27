d43cc95f8502f1197e3795e22578c8e96d59d154
E1 <noemail@example.org>
2020-08-27 12:37:34 +0800

    _

--- archive-uploader/sources.list.sh
+++ archive-uploader/sources.list.sh
@@ -4,6 +4,11 @@ TIME=`date +%s` || {
 	exit 1
 }
 
+apt install apt-transport-https -y || {
+	echo 'apt failed '$?
+	exit 4
+}
+
 cp /etc/apt/sources.list '/etc/apt/sources.list.'$TIME'.bak' || {
 	echo 'cp backup failed '$?
 	exit 2
